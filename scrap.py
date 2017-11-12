912ccc29c09e02cdb4b8315579bc855de570b1d2
panagiotis <p.h.adamopoulos@gmail.com>
2017-11-12 10:52:58 +0200

    Some scrap prints for dictionaries and lists

---
+++ scrap.py
@@ -0,0 +1,10 @@
+adict = {1:'A', 20:'B', 50:'C'}
+
+print(sum([max(adict), min(adict)]))
+
+print(sorted([max(adict), len(adict), sum(adict), min(adict)]))
+
+print(len([max(adict), len(adict), min(adict)]))
+
+bdict = [max(adict) if i%2 else min(adict) for i in range(4)]
+print(bdict)
